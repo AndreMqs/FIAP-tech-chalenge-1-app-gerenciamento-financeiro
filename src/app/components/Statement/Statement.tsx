4b4ade3dbc2bdb87f449922ad4ce022aed837d1b
Andre Camara <andre.m.camara@hotmail.com>
2024-10-25 00:47:19 -0300

    Separating statements by months

--- src/app/components/Statement/Statement.tsx
+++ src/app/components/Statement/Statement.tsx
@@ -1,6 +1,12 @@
 import cn from 'classnames';
 
-import SingleStatement from './SingleStatement/SingleStatement';
+import IconButton from '@mui/material/IconButton';
+
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import StatementList from './StatementList/StatementList';
+
+import { getStatementByMonth } from '@/app/utils/statementUtils';
 
 import styles from "./Statement.module.scss"
 import globalStyles from "../../globals.module.scss";
@@ -9,21 +15,73 @@ import globalStyles from "../../globals.module.scss";
 export default function Statement(props: StatementProps) {
   const {} = props;
 
+  const statementsList = [
+    {
+      type: 'Depósito',
+      date: new Date('2024-01-15'),
+      moneyValue: 150,
+    },
+    {
+      type: 'Depósito',
+      date: new Date('2024-01-15'),
+      moneyValue: 1501,
+    },
+    {
+      type: 'Depósito',
+      date: new Date('2024-02-15'),
+      moneyValue: 1502,
+    },
+    {
+      type: 'Depósito',
+      date: new Date('2024-03-15'),
+      moneyValue: 1503,
+    },
+    {
+      type: 'Depósito',
+      date: new Date('2024-03-15'),
+      moneyValue: 1504,
+    },
+    {
+      type: 'Depósito',
+      date: new Date('2024-03-15'),
+      moneyValue: 1504,
+    },
+    {
+      type: 'Depósito',
+      date: new Date('2024-04-15'),
+      moneyValue: 1504,
+    },
+    {
+      type: 'Depósito',
+      date: new Date('2024-07-15'),
+      moneyValue: 1504,
+    },
+    {
+      type: 'Depósito',
+      date: new Date(),
+      moneyValue: 1504,
+    },
+  ]
+
   return (
     <div id='statement' className={cn(styles.statementContainer, globalStyles.borderTest)}>
       <div className={styles.statementHeader}>
         <span className={styles.headerTitle}>Extrato</span>
         <span>
-          <button className={styles.headerButton}>E</button>
-          <button className={styles.headerButton}>D</button>
+          <IconButton className={styles.headerButton}>
+            <EditIcon />
+          </IconButton>
+          <IconButton className={styles.headerButton}>
+            <DeleteIcon />
+          </IconButton>
         </span>
       </div>
-      <SingleStatement 
-        type='Depósito'
-        date={new Date()}
-        moneyValue={150}
+      <div className={styles.statementsListContainer}>
+      <StatementList 
+        statementsByMonth={getStatementByMonth(statementsList)}
         isEditing={true}
       />
+      </div>
     </div>
   );
 }
--- src/app/components/TextField/TextField.tsx
+++ src/app/components/TextField/TextField.tsx
@@ -1,14 +1,16 @@
+import cn from "classnames";
 import { TextField as MuiTextField, TextFieldVariants} from "@mui/material";
 
 import styles from "./TextField.module.scss"
 
 
 export default function TextField(props: SingleStatementProps) {
-  const {value, variant} = props;
+  const {value, variant, style} = props;
   
   return (
     <MuiTextField 
-      className={styles.textField}
+      style={style}
+      className={cn('textField', styles.textField)}
       value={value}
       variant={variant ?? 'standard'}
     />
@@ -19,4 +21,5 @@ export default function TextField(props: SingleStatementProps) {
 interface SingleStatementProps {
   value?: string;
   variant?: TextFieldVariants;
+  style?: any;
 }
