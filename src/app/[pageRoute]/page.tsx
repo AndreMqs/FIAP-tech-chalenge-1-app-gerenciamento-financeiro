9f20fcafe49e30ba49ad8dbc587c335bcaf21131
Andre Camara <andre.m.camara@hotmail.com>
2024-10-30 23:38:10 -0300

    Improving ssr components

--- src/app/[pageRoute]/page.tsx
+++ src/app/[pageRoute]/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { isNil } from "lodash";
 import HomePage from "../components/HomePage/HomePage";
 import MainPage from "../components/MainPage/MainPage";
--- src/app/components/HomePage/HomePage.tsx
+++ src/app/components/HomePage/HomePage.tsx
@@ -1,4 +1,3 @@
-'use client'
 import dynamic from 'next/dynamic';
 
 const HeaderHomePage = dynamic(() => import('./HeaderHomePage/HeaderHomePage'), { ssr: false });
